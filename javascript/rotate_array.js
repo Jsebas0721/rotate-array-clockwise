fb3a19d0e989be95bb00a1e04ddd61bceb69bc33
sebastian medina <sebastianlopez_21@hotmail.com>
2023-11-12 23:38:53 -0500

    Challenge resolved

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -1,9 +1,29 @@
 function rotateArray(arr, k) {
   // type your code here
+
+  if(arr.length === k || arr.length == [] || k === 0){
+    return arr;
+  }else{
+    for(let i = 0; i < k; i++){
+      arr.unshift(arr.pop());
+    }
+  }
+
+  console.log(arr)
+    
+ return arr;
 }
+
+
+
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: [3,4, 5, 1, 2]");
+  console.log("=>", rotateArray([1, 2, 3, 4, 5], 3));
+
+  console.log("");
+
   console.log("Expecting: [4, 1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3, 4], 1));
 
@@ -16,6 +36,11 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3], 3));
+
+  console.log("");
+
+  console.log("Expecting: [1, 2, 3]");
+  console.log("=>", rotateArray([1, 2, 3], 0));
 }
 
 module.exports = rotateArray;
